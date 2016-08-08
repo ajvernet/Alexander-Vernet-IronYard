334e3143ab6feb0a714d6f12381237b4fac43b86
Alexander Vernet <alexanderjvernet@gmail.com>
2016-08-08 08:53:44 -0400

    updated java formatting for Knight files

--- src/main/java/com/tiy/ssa/weekone/assignmentfour/Position.java
+++ src/main/java/com/tiy/ssa/weekone/assignmentfour/Position.java
@@ -1,37 +1,36 @@
 package com.tiy.ssa.weekone.assignmentfour;
 
 public class Position {
-	public final int x;
-	public final int y;
-	
-	public Position(int x, int y){
-		this.x = x;
-		this.y = y;
-	}
+    public final int x;
+    public final int y;
 
-	
-	public boolean equals(Position other) {
-		if(x == other.x && y == other.y)
-			return true;
-		
-		return false;
-	}
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
 
-	public Position move(int x, int y){
-		
-		return new Position(this.x + x, this.y + y);
-	}
-	
-	public int getX(){
-		return x;
-	}
-	
-	public int getY(){
-		return y;
-	}
-	
-	@Override
-	public String toString(){
-		return "Position is: (" + x + ", " + y + ").";
-	}
+    public boolean equals(Position other) {
+        if (x == other.x && y == other.y)
+            return true;
+
+        return false;
+    }
+
+    public Position move(int x, int y) {
+
+        return new Position(this.x + x, this.y + y);
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    @Override
+    public String toString() {
+        return "Position is: (" + x + ", " + y + ").";
+    }
 }
--- src/test/java/com/tiy/ssa/weekone/assignmentfour/LazyKnightTest.java
+++ src/test/java/com/tiy/ssa/weekone/assignmentfour/LazyKnightTest.java
@@ -6,43 +6,42 @@ import org.junit.Test;
 
 public class LazyKnightTest {
 
-	final int maxMoves = 4;
-	
-	@Test
-	public void testMoveNumber() {
-		LazyKnight sirThurston = new LazyKnight(4, 2);
-		
-		for(int i = 0; i < maxMoves; i++){
-			sirThurston.move();
-		}
-		
-		assertTrue(sirThurston.position.equals(sirThurston.home));
-	}
+    final int maxMoves = 4;
 
-	@Test
-	public void legalMove(){
-		LazyKnight sirThurston = new LazyKnight(1, 10);
-		
-		sirThurston.move();
-		
-		assertTrue(4 > sirThurston.getPosition().getX() && sirThurston.getPosition().getX() > -2);
-		assertTrue(13 > sirThurston.getPosition().getY() && sirThurston.getPosition().getY() > 7);
-	}
-	
-	@Test
-	public void notRetraced(){
-		
-		Position lastPosition;
-		LazyKnight sirThurston = new LazyKnight(1, 10);
-		
-		for(int i = 0; i < 10; i++){	
-			lastPosition = sirThurston.getPosition();		
-			sirThurston.move();
-			System.out.println(sirThurston.toString());
-			assertFalse(sirThurston.move().equals(lastPosition));
-			
-		}
-	}
-	
-	
+    @Test
+    public void testMoveNumber() {
+        LazyKnight sirThurston = new LazyKnight(4, 2);
+
+        for (int i = 0; i < maxMoves; i++) {
+            sirThurston.move();
+        }
+
+        assertTrue(sirThurston.position.equals(sirThurston.home));
+    }
+
+    @Test
+    public void legalMove() {
+        LazyKnight sirThurston = new LazyKnight(1, 10);
+
+        sirThurston.move();
+
+        assertTrue(4 > sirThurston.getPosition().getX() && sirThurston.getPosition().getX() > -2);
+        assertTrue(13 > sirThurston.getPosition().getY() && sirThurston.getPosition().getY() > 7);
+    }
+
+    @Test
+    public void notRetraced() {
+
+        Position lastPosition;
+        LazyKnight sirThurston = new LazyKnight(1, 10);
+
+        for (int i = 0; i < 10; i++) {
+            lastPosition = sirThurston.getPosition();
+            sirThurston.move();
+            System.out.println(sirThurston.toString());
+            assertFalse(sirThurston.move().equals(lastPosition));
+
+        }
+    }
+
 }
