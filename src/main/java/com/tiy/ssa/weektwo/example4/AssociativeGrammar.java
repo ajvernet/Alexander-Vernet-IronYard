fb342291d5512614747400555f755264d365e2db
Alexander Vernet <alexanderjvernet@gmail.com>
2016-08-10 08:54:42 -0400

    committing name and user (forgot to yesterday)

--- src/main/java/com/tiy/ssa/weektwo/example4/AssociativeGrammar.java
+++ src/main/java/com/tiy/ssa/weektwo/example4/AssociativeGrammar.java
@@ -2,4 +2,24 @@ package com.tiy.ssa.weektwo.example4;
 
 public class AssociativeGrammar {
 
+    private String input;
+
+    public AssociativeGrammer(String input){
+        this.input = input;
+    }
+    
+    public String getSource(){
+        return input;
+    }
+    
+    public String addOperator(char operator){
+        return input += operator;
+    }
+    
+    public boolean compiles(){
+        
+        char[] validChars = {'{', '}', '(', ')', '[', ']'};
+        return false;
+        
+    }
 }
